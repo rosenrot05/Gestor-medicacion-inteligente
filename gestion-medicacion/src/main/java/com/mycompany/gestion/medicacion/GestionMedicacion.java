c6f5ef897b7de9a8107249dd549cf0ad0d38b9e3
rosenrot05 <rosamariapca10@gmail.com>
2026-05-13 01:40:26 -0500

    Primer Commit del proyecto

---
+++ gestion-medicacion/src/main/java/com/mycompany/gestion/medicacion/GestionMedicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestion.medicacion;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class GestionMedicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
